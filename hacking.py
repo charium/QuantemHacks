9f2b588bb1810d5d88cb47a25fd1680d58ca44ba
Chloe Hall <ha11@ad.unc.edu>
2025-04-12 19:01:57 -0400

    Clean up repository

--- hacking.py
+++
@@ -1,8 +0,0 @@
-#Generate numbers OR bit strings based on data about people (likes vs dislikes)
-#Write function to measure "compatibility" where the higher the funciton's output, the more compatible
-#Optimize function to find best match
-#Remove best match from pool
-#Rerun
-
-print("testing")
-
